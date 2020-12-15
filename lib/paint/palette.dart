06e38f57a36e5d1453fa457595dd538ed4594542
VRedBull <pradhanvikas11@gmail.com>
2020-12-15 19:32:00 +0530

    added colors

--- lib/paint/palette.dart
+++ lib/paint/palette.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/painting.dart';
 
 class Palette {
-  static const Color darkBlue = Color(0xff092E34);
-  static const Color lightBlue = Color(0xff489FB5);
-  static const Color orange = Color(0xffFFA62B);
+  static const Color darkBlue = Color(0xff092E34); //skin color
+  static const Color lightBlue = Color(0xff489FB5); //black shade
+  static const Color orange = Color(0xffFFA62B); //
   static const Color darkOrange = Color(0xffCC7700);
 }
