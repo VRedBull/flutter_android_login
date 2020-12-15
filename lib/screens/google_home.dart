e7543738eb5e6fa3e417e795749f168227784f6e
VRedBull <pradhanvikas11@gmail.com>
2020-12-15 19:49:25 +0530

    error google auth

--- lib/screens/google_home.dart
+++ lib/screens/google_home.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_android/blocs/google_sign_in.dart';
 import 'package:flutter_login_android/paint/background_painter.dart';
 import 'package:flutter_login_android/screens/login.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:provider/provider.dart';
 import 'login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -26,7 +29,9 @@ class _GoogleHomeState extends State<GoogleHome> with SingleTickerProviderStateM
     super.dispose();
   }
 
-  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+  final user = FirebaseAuth.instance.currentUser;
+
+  // GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +46,7 @@ class _GoogleHomeState extends State<GoogleHome> with SingleTickerProviderStateM
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Vikas',
+              user.displayName,
               style: TextStyle(fontSize: 25),
             ),
             SizedBox(
@@ -64,10 +69,14 @@ class _GoogleHomeState extends State<GoogleHome> with SingleTickerProviderStateM
                 Buttons.Google,
                 text: 'Sign out of Google',
                 onPressed: () {
-                  _googleSignIn
-                      .signOut()
-                      .then((user) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login())));
+                  final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+                  provider.logout();
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login()));
                   _controller.forward(from: 0);
+                  // _googleSignIn
+                  //     .signOut()
+                  //     .then((user) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login())));
+                  // _controller.forward(from: 0);
                 },
                 padding: EdgeInsets.symmetric(vertical: 2, horizontal: 2),
               ),
